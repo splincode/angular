180cdaf1bf99251b49948c2d073423dd7fc1fba3
Максим Иванов <omaxphp@yandex.ru>
2026-04-11 03:04:49 +0900

    docs(compiler): clarify reduceUselessStaticTemplates scope
    Document that reduceUselessStaticTemplates currently optimizes only @if blocks and does not yet apply to @switch or @for.

---
+++ packages/compiler-cli/test/compliance/test_cases/r3_view_compiler_control_flow/if_static_true_template.js
@@ -0,0 +1,24 @@
+function MyApp_Conditional_0_Template(rf, ctx) {
+  if (rf & 1) {
+    $r3$.ɵɵelementStart(0, "p");
+    $r3$.ɵɵtext(1, "Hello");
+    $r3$.ɵɵelementEnd();
+  }
+}
+
+function MyApp_Conditional_1_Template(rf, ctx) {
+  if (rf & 1) {
+    $r3$.ɵɵelementStart(0, "p");
+    $r3$.ɵɵtext(1, "world");
+    $r3$.ɵɵelementEnd();
+  }
+}
+…
+function MyApp_Template(rf, ctx) {
+  if (rf & 1) {
+    $r3$.ɵɵconditionalCreate(0, MyApp_Conditional_0_Template, 2, 0)(1, MyApp_Conditional_1_Template, 2, 0);
+  }
+  if (rf & 2) {
+    $r3$.ɵɵconditional(true ? 0 : 1);
+  }
+}
